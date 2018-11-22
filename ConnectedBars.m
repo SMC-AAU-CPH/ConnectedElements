04ce78dc13b625f41d472f5ffbcee4c4fd29c78d
SilvinWillemsen <silvinwillemsen@gmail.com>
2018-11-22 16:14:19 +0100

    Added bowed string

--- ConnectedBars.m
+++ ConnectedBars.m
@@ -14,9 +14,9 @@ objectVars{1, 1} = "string";
 freq1 = 196.0;
 objectVars{1, 2} = [freq1*2, 2, 0.1, 0.005];
 
-objectVars{2, 1} = "string";
-freq2 = 196.0*2^(7/12);
-objectVars{2, 2} = [freq2*2, 2, 0.1, 0.005];
+objectVars{2, 1} = "bowedString";
+freq2 = 110;
+objectVars{2, 2} = [freq1*2, 2, 0.1, 0.005];
 
 % objectVars{3, 1} = "plate";
 % objectVars{3, 2} = [1, 1, 0.005, 4];
@@ -26,26 +26,41 @@ Q = length(objectVars); % amount of objects
 %initialise cells/vectors depending on the number of objects
 Bpre = cell(Q, 1);
 Cpre = cell(Q, 1);
+
+bB = cell(Q, 1);
+bC = cell(Q, 1);
+
 N = zeros(Q,1);
 h = zeros(Q,1);
 matIdx = zeros(Q,1);
 
+Nx = cell(Q, 1);
+Ny = cell(Q, 1);
+
 Ntot = 0;
-numPlates = 0;
+
+whatExciter = "normal";
+
 for q = 1:Q
     
     if objectVars{q, 1} == "bar"
         [Bpre{q}, Cpre{q}, N(q), h(q), s0(q)] = createBar(objectVars{q, 2}, fs);
     elseif objectVars{q, 1} == "string"
         [Bpre{q}, Cpre{q}, N(q), h(q), s0(q)] = createString(objectVars{q, 2}, fs);
+    elseif objectVars{q, 1} == "bowedString"
+        whatExciter = "bowed";
+        [Bpre{q}, Cpre{q}, bB{q}, bC{q}, N(q), h(q), s0(q)] = createBowedString(objectVars{q, 2}, fs);
     elseif objectVars{q, 1} == "plate"
-        numPlates = numPlates + 1;
-        [Bpre{q}, Cpre{q}, N(q), h(q), s0(q), Nx(numPlates), Ny(numPlates)] = createPlate(objectVars{q, 2}, fs);
+        [Bpre{q}, Cpre{q}, N(q), h(q), s0(q), Nx{q}, Ny{q}] = createPlate(objectVars{q, 2}, fs);
     end
     matIdx(q) = Ntot + 1;
     Ntot = Ntot + N(q);
 end
 
+vec = cell(Q, 1);
+for q = 1:Q
+    vec{q} = matIdx(q):matIdx(q)+N(q)-1;
+end
 
 %% Create full matrices
 B = zeros(Ntot);
@@ -63,37 +78,43 @@ uPrev = u;
 uNext = u;
 
 %% Length of the sound
-lengthSound = fs*5;
+lengthSound = fs*10;
+
 
 %% Exciter
-Fmax = 500;
-exciterLength = floor(1e-3 * fs);
-v = 2; 
-Fe = Fmax/2*(1-cos(v*pi*(0:exciterLength)/exciterLength));
-exciter = [Fe, zeros(1,lengthSound-exciterLength - 1)];
-
-OTE = 2; % Object To Excite
-exciterPos = 1/2;
-rcW = 3;
-
-if objectVars{OTE,1} == "plate"
-%     Ee = zeros(Ntot, 1);
-%     Ee (matIdx(OTE) + exciterPos*N(OTE)-floor(rcW/2):matIdx(OTE) + exciterPos*N(OTE)+ceil(rcW/2)) = 1-cos(v*pi*(0:rcW)/rcW);
-%     K = k^2 * Ee / (1 + s0(OTE)*k);
+if whatExciter == "bowed"
+    %Bow Model
+    a = 100;                % free parameter
+    BM = sqrt(2*a)*exp(1/2);
+    Fb = 5;
+    Vb = 0.2;
+    Ee = zeros(Ntot, 1);
+    Ee(N(1) + floor(N(2)/4)) = 1;
+    K = k^2 * Ee / (1 + s0(2)*k);
+    vrelPrev = 0;
+    tol = 1e-4;
 else
+    Fmax = 500;
+    exciterLength = floor(1e-3 * fs);
+    v = 2; 
+    Fe = Fmax/2*(1-cos(v*pi*(0:exciterLength)/exciterLength));
+    exciter = [Fe, zeros(1,lengthSound-exciterLength - 1)];
+
+    OTE = 2; % Object To Excite
+    exciterPos = 1/2;
+    rcW = 3;
+
     Ee = zeros(Ntot, 1);
     Ee (matIdx(OTE) + exciterPos*N(OTE)-floor(rcW/2):matIdx(OTE) + exciterPos*N(OTE)+ceil(rcW/2)) = 1-cos(v*pi*(0:rcW)/rcW);
     K = k^2 * Ee / (1 + s0(OTE)*k);
 end
 %% Connections
 
-% 1st, 2nd object,
-% location of connection at 1st, 2nd object
-% width of connection at 1st, 2nd object
-conn = [1, 2, 0.5, 0.3, 5, 5];
+% 1st, 2nd object, 1st, 2nd location of connection and 1st, 2nd width of connection
+conn = [1, 2, 0.5, 0.25, 1, 1];
 %         1, 2, 0.9, 0.6, 1, 1];
 %         1, 2, 0.9, 0.5, 1, 1];
-    
+
 Qc = length(conn(:,1)); % amount of connections
 J = zeros(Ntot, Qc);
 massRatio = 1; 
@@ -120,8 +141,8 @@ w0 = zeros(Qc, 1);
 w1 = zeros(Qc, 1);
 
 sx(1) = 0;
-w0(1) = 1; 
-w1(1) = 1; 
+w0(1) = 10; 
+w1(1) = 10; 
 
 % sx(2) = 0;
 % w0(2) = 1; 
@@ -137,20 +158,8 @@ out2 = zeros(lengthSound, 1);
 drawBar = false;
 
 etaRPrev = zeros(Qc, 1);
+            
 
-% Plotstuff
-% if length(connA) == 1
-%     scatA = true;
-% else
-%     scatA = false;
-% end 
-% 
-% if length(connB) == 1
-%     scatB = true;
-% else
-%     scatB = false;
-% end 
-% plateMat = zeros(Ny-1, Nx-1);
 for n = 1 : lengthSound
     
     % calculate relative displacement
@@ -163,9 +172,28 @@ for n = 1 : lengthSound
     Rn = eye(Qc).*rn; 
     Pn = eye(Qc).*pn; 
     
-    % temporary value for the next u (without the spring forces)
-    uTemp = B * u + C * uPrev + K*exciter(n);
-    
+    if whatExciter == "bowed"
+        % Newton-Raphson
+        b = 2/k * Vb + 2 * s0(2) * Vb + Ee(vec{2})' * bB{2} * u(vec{2}) + Ee(vec{2})' * bC{2} * uPrev(vec{2});
+        eps = 1;
+        i = 0;
+        while eps>tol
+            vrel=vrelPrev-(Fb*BM*vrelPrev*exp(-a*vrelPrev^2)+2*vrelPrev/k+2*s0(2)*vrelPrev+b)/...
+             (Fb*BM*(1-2*a*vrelPrev^2)*exp(-a*vrelPrev^2)+2/k+2*s0(2));
+            eps = abs(vrel-vrelPrev);
+            vrelPrev = vrel;
+            i = i + 1;
+            if i > 10000
+                disp('Nope');
+            end
+        end
+        vrelSave(n) = vrel;
+        excitation = k^2*Ee'*1/h(2)*Fb*BM*vrel*exp(-a*vrel^2);
+        uTemp = B * u + C * uPrev - excitation';
+    else
+        % temporary value for the next u (without the spring forces)
+        uTemp = B * u + C * uPrev + K*exciter(n);
+    end 
     % find Fc
     bn = L * uTemp; 
     an = Rn * etaRPrev; 
@@ -175,18 +203,13 @@ for n = 1 : lengthSound
     uNext = uTemp + J * Fc;
     
     out(n) = u(round(N(1)/5));
-    out2(n) = u(round(Ntot - 5));
+    out2(n) = u(round(Ntot - 15));
     if mod(n,1) == 0 && drawBar == true
         subplot(2,1,1)
-        plot(u(matIdx(1):matIdx(1)+N(1)-1))
+        plot(u(vec{1}))
 %         u = ones(length(u),1);
         subplot(2,1,2)
-        plot(u(matIdx(2):matIdx(2)+N(2)-1))
-%         for it = 1:Nx-1
-%             plateMat(:,it) = u(matIdx(3) + (it - 1) * (Ny-1): matIdx(3) + it * (Ny-1) - 1);
-%         end
-%         surf(plateMat, 'FaceAlpha', 0.5);
-%         zlim([-0.5e50 0.5e50])
+        plot(u(vec{2}))
         drawnow;
     end
     uPrev = u;
