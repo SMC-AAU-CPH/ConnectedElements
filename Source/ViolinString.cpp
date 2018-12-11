5d3725dbf79d5ce85197f21939866f122448a362
Nikolaj Andersson <nikolajandersson@gmail.com>
2018-12-11 11:33:16 +0100

    excite symp strings

--- Source/ViolinString.cpp
+++ Source/ViolinString.cpp
@@ -291,7 +291,7 @@ void ViolinString::setRaisedCos (double exciterPos, double width)
     int j = 0;
     for (int i = floor(exciterPos*N) - floor(width / 2.0); i < floor(exciterPos*N) + floor(width / 2.0); ++i)
     {
-        u[i] = (1 - cos(2 * double_Pi * j / width)) * 0.5;
+        u[i] = (1 - cos(1 * double_Pi * j / width)) * 0.5;
         uPrev = u;
         ++j;
     }
